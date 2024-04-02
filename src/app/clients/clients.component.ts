90c9352d45b2c1aeecc0a551f947605037dae90d
luana.arruda <luanamarrudaa@gmail.com>
2024-04-01 22:17:38 -0300

    Add reactive form

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Student } from '../student';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-clients',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './clients.component.css'
 })
 export class ClientsComponent {
+  students: Student[] = []; //array
+
+  formGroupStudent : FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.formGroupStudent = formBuilder.group({
+      id : [''],
+      name : [''],
+      course : ['']
+    });
+  }
+
+  save(){
+    this.students.push(this.formGroupStudent.value);
+  }
 
 }
